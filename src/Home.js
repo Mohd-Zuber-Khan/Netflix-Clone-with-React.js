baedefcbb530cbae43454aba14b3761c93bad103
zuber <kzuberkhan0786@gmail.com>
2023-05-03 13:04:44 +0530

    Netflix-Clone-React-js

---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Row from './Row'
+import requests from './requests'
+import Banner from './Banner'
+import Header from './Header'
+
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Row endPoint={requests.fetchNetflixOriginals} heading='Netflix Original' isBigger={true} />
+      <Row endPoint={requests.fetchTrending} heading='Tranding Now' />
+      <Row endPoint={requests.fetchTopRated} heading='Top Rated' />
+      <Row endPoint={requests.fetchActionMovies} heading='Action Movies' />
+      <Row endPoint={requests.fetchComedyMovies} heading='Comedy Movies' />
+      <Row endPoint={requests.fetchHorrorMovies} heading='Horror Movies' />
+      <Row endPoint={requests.fetchRomanceMovies} heading='Romantic Movies' />
+      <Row endPoint={requests.fetchDocumentaries} heading='Documentaries' />
+    </>
+
+  )
+}
+
+export default Home
+
+
